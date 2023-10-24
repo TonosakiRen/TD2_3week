036495b672e3890ea796085a320db6d8a2943cc7
TonosakiRen <ren221603@gmail.com>
2023-10-24 18:38:17 +0900

    bossEffect

---
+++ BossExplode.cpp
@@ -0,0 +1,85 @@
+#include "BossExplode.h"
+#include "ImGuiManager.h"
+
+BossExplode::BossExplode()
+{
+	particleBox_ = std::make_unique<ParticleBox>(kParticleNum);
+}
+
+void BossExplode::Initialize()
+{
+
+	particleBox_->Initialize();
+	emitterWorldTransform_.SetIsScaleParent(false);
+	emitterWorldTransform_.scale_ = { 10.0f ,10.0f,10.0f};
+	minDirection_ = { -1.0f,-1.0f ,-1.0f };
+	maxDirection_ = { 1.0f,1.0f,1.0f };
+
+}
+
+void BossExplode::Update() {
+
+	emitterWorldTransform_.UpdateMatrix();
+
+	emitFrame_--;
+
+	if (isEmit_ == true) {
+		emitFrame_ = kEmitFrame_;
+		isEmit_ = false;
+	}
+
+	if (emitFrame_ > 0) {
+		for (size_t i = 0; i < EmitNum_; i++) {
+			for (size_t i = 0; i < kParticleNum; i++) {
+				if (particles[i].isActive_ == false) {
+					particles[i].isActive_ = true;
+					if (emitterWorldTransform_.GetParent()) {
+						particles[i].direction_ = Normalize(Vector3{ Rand(minDirection_.x, maxDirection_.x) ,Rand(minDirection_.y,maxDirection_.y) ,Rand(minDirection_.z,maxDirection_.z) } *NormalizeMakeRotateMatrix(emitterWorldTransform_.GetParent()->matWorld_));
+					}
+					else {
+						particles[i].direction_ = Normalize({ Rand(minDirection_.x, maxDirection_.x) ,Rand(minDirection_.y,maxDirection_.y) ,Rand(minDirection_.z,maxDirection_.z) });
+					}
+					particles[i].worldTransform_.translation_ = MakeTranslation(emitterWorldTransform_.matWorld_);
+					particles[i].worldTransform_.rotation_ = { 0.0f,0.0f,0.0f };
+					particles[i].worldTransform_.scale_ = emitterWorldTransform_.scale_;
+					break;
+				}
+			}
+		}
+	}
+
+	for (size_t i = 0; i < kParticleNum; i++) {
+		float rotationSpeed = Radian(1.0f) * (float(i % 2) * 2.0f - 1.0f);
+		if (particles[i].isActive_ == true) {
+			particles[i].worldTransform_.rotation_ = particles[i].worldTransform_.rotation_ + rotationSpeed;
+			particles[i].worldTransform_.translation_ += particles[i].direction_ * speed_;
+			particles[i].worldTransform_.scale_ = particles[i].worldTransform_.scale_ - scaleSpeed_;
+			if (particles[i].worldTransform_.scale_.x <= 0.0f) {
+				particles[i].isActive_ = false;
+			}
+		}
+
+	}
+
+}
+
+void BossExplode::Draw(ViewProjection* viewProjection, DirectionalLight* directionalLight, Vector4 color,uint32_t textureHandle)
+{
+
+	emitterWorldTransform_.UpdateMatrix();
+
+	std::vector<ParticleBox::InstancingBufferData> instancingBufferDatas;
+	instancingBufferDatas.reserve(kParticleNum);
+
+	for (size_t i = 0; i < kParticleNum; i++)
+	{
+		if (particles[i].isActive_) {
+			particles[i].worldTransform_.UpdateMatrix();
+			instancingBufferDatas.emplace_back(particles[i].worldTransform_.matWorld_);
+		}
+	}
+
+	if (!instancingBufferDatas.empty()) {
+		particleBox_->Draw(instancingBufferDatas, *viewProjection, *directionalLight, color, textureHandle);
+	}
+}
--- GameScene.cpp
+++ GameScene.cpp
@@ -65,6 +65,10 @@ void GameScene::Initialize() {
 
 	explodePlayerParticle_.Initialize();
 	explodeBossParticle_.Initialize();
+
+	bossExplode_.Initialize();
+
+	blockHandle_ = TextureManager::Load("block.png");
 }
 
 void GameScene::Update(){
@@ -163,6 +167,14 @@ void GameScene::Update(){
 	
 	viewProjection_.Shake({ 3.0f,3.0f,3.0f }, explodeFrame_);
 
+	bossExplodeFrame--;
+	if (bossExplodeFrame > 0) {
+		bossExplode_.SetIsEmit(true);
+	}
+	else {
+		bossExplode_.SetIsEmit(false);
+	}
+	bossExplode_.Update();
 	for (int i = 0; i < pillarNum; i++) {
 		pillar_[i].Update();
 	}
@@ -220,6 +232,7 @@ void GameScene::ParticleBoxDraw()
 	explodeBossParticle_.Draw(&viewProjection_, &directionalLight_, { 1.0f,0.0f,0.0f,1.0f });
 	collapse_.Draw(&viewProjection_, &directionalLight_, { 121.0f / (255.0f * 4.0f), 101.0f / (255.0f * 4.0f), 53.0f / (255.0f * 4.0f) });
 	orbit_.Draw(&viewProjection_, &directionalLight_, { 0.5f,0.5f,0.5f,1.0f });
+	bossExplode_.Draw(&viewProjection_, &directionalLight_, { 1.0f,1.0f,1.0f,1.0f },blockHandle_);
 	for (const auto& bullet : enemyBullets_) {
 		bullet->ParicleDraw();
 	}
@@ -270,7 +283,6 @@ void GameScene::Draw() {
 
 void GameScene::CollisionCheck() {
 
-	tmpCollider_.AdjustmentScale();
 
 	//敵弾とプレイヤーとの衝突判定
 	bool isHitBulee = false;
@@ -563,8 +575,13 @@ void GameScene::InGameUpdate() {
 	boss_[0]->Update();
 
 	if (boss_[0]->IsDead()) {
+		bossExplode_.emitterWorldTransform_.translation_= boss_[0]->GetWorldPos();
 		BossPopComand();
 		//Boss::isBreak_ = false;
+		bossExplodeFrame = 10;
+	}
+	if (input_->TriggerKey(DIK_Y)) {
+		bossExplodeFrame = 10;
 	}
 
 	for (const auto& bullet : enemyBullets_) {
